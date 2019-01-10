aef8403273b3fd3fa5fa7818a36fcb64c1dafc6f
Sean Anderson <sean@seananderson.ca>
2019-01-10 14:28:06 -0800

    Add here::here; tweak herring script .R -> .Rmd

--- plots/plot-preamble.R
+++ plots/plot-preamble.R
@@ -9,16 +9,16 @@ if (!exists("user"))
     "on the command line to load the correct (confidential) raw data.", call. = FALSE)
 
 if (user[[1]] == "Eric")
-  load("/users/eric.ward/documents/CFEC/data/cfec_070616")
+  load(here::here("/users/eric.ward/documents/CFEC/data/cfec_070616"))
 if (user[[1]] == "Sean")
-  load("../pfx-commercial/data/cfec_070616")
+  load(here::here("../pfx-commercial/data/cfec_070616"))
 if (user[[1]] == "Jordan")
-  load("../cfec_070616.Rdata")
+  load(here::here("../cfec_070616.Rdata"))
 
 if (!exists("cfec")) stop("The data frame `cfec` must be loaded first.")
 
 # Deflate the data per Sean's paper
-deflationTable <- read.csv("data/deflation.csv")
+deflationTable <- read.csv(here::here("data/deflation.csv"))
 # Adjust price and g_earn for inflation
 cfec$year <- as.numeric(cfec$year)
 cfec$day <- substr(cfec$landdate, 6, 10)
@@ -27,7 +27,7 @@ cfec <- mutate(cfec, g_price = g_price / defl, g_earn = g_earn / defl)
 cfec$defl <- NULL
 
 #  Get Anne's regions
-region <- read.csv("data/regions.csv")
+region <- read.csv(here::here("data/regions.csv"))
 cfec <- cfec %>%
   inner_join(region[, c("stat6", "final")]) %>%
   rename(region = final)
